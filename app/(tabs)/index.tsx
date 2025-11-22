66da48ec91a8e6ed66fbab8c5cfe61061ef9165f
Palatip Boonmeerit <pboonmeerit@student.unimelb.edu.au>
2025-11-22 16:58:33 +1100

    Day 1: Welcome screen completed - logo with Poppins font - catch phrase - create account and sign in button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,115 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { Poppins_700Bold, useFonts } from "@expo-google-fonts/poppins";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const [fontLoaded] = useFonts({
+    Poppins_700Bold,
+  });
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  if (!fontLoaded) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0cc0df" />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <Text style={styles.titleLogoFont} allowFontScaling={true}>
+          mfit.
+        </Text>
+      </View>
+
+      <View style={styles.middleBar}>
+        <View style={styles.MiddleCatchPhrase}>
+          <Text style={styles.catchPhraseFont}>
+            BECAUSE FITNESS STARTS WITH YOU.
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.bottomBar}>
+        <TouchableOpacity style={styles.GS_Button}>
+          <Text style={styles.GSAppFont}>Get Started</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.or}>or</Text>
+
+        <TouchableOpacity style={styles.GS_Button}>
+          <Text style={styles.GSAppFont}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  topBar: {
+    flex: 1,
+    flexDirection: "row",
+    width: '100%',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    alignItems: 'flex-start',
+  },
+  middleBar: {
+    flex: 1,
+  },
+  bottomBar: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 25,
+  },
+  titleLogoFont: {
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "#0cc0df",
+    fontFamily: "Poppins_700Bold",
+  },
+  GSAppFont: {
+    fontWeight: "bold",
+    fontSize: 24,
+    color: "white",
+    fontFamily: "Poppins_700Bold",
+  },
+
+  MiddleCatchPhrase: {
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  catchPhraseFont: {
+    fontWeight: "bold",
+    fontSize: 24,
+    color: "#0cc0df",
+    fontFamily: "Poppins_700Bold",
+  },
+  GS_Button: {
+    backgroundColor: "#0cc0df",
+    borderColor: "#0cc0df",
+    borderWidth: 15,
+    borderRadius: 30,
+  },
+  or: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 16,
   },
 });
